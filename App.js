3526106f56e52acc1ec0cffb86f6cd1cc974d2b8
Alexander Oguejiofor <oguejioforalexander@gmail.com>
2021-01-22 19:22:36 +0100

    feat: index page

--- App.js
+++ App.js
@@ -1,13 +1,18 @@
-import { StatusBar } from 'expo-status-bar'
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+import IndexScreen from './src/screens/IndexScreen'
+
+const Stack = createStackNavigator()
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your cunt!</Text>
-      <StatusBar style='auto' />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name='Index' component={IndexScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   )
 }
 
---
+++ src/screens/IndexScreen.js
@@ -0,0 +1,18 @@
+import React, { useContext, useEffect } from 'react'
+import {
+  Text,
+  View,
+  StyleSheet,
+  FlatList,
+  TouchableOpacity,
+} from 'react-native'
+
+const IndexScreen = () => {
+  return (
+    <View>
+      <Text>Index</Text>
+    </View>
+  )
+}
+
+export default IndexScreen
